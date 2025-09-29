4d40037bdfe6016eb655cc9e8b1c5d6991659745
Bhavesh Sonar <bhaveshsonar003@gmail.com>
2025-09-29 22:19:42 +0530

    update urls

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -20,7 +20,7 @@ const Editor = () => {
       if (!token) {
         throw new Error("No token found");
       }
-      await fetch("http://15.207.221.31:1234/api/profile", {
+      await fetch("https://15.207.221.31:1234/api/profile", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
